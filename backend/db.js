8287bc3bad98fdade48cb34d2e0f77696389ec04
goodman97 <151006064+goodman97@users.noreply.github.com>
2026-03-23 23:32:24 +0700

    minor update

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
-//require('dotenv').config();
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
@@ -13,8 +13,8 @@ const db = mysql.createConnection({
   host: 'localhost', 
   user: 'root',
   password: '', 
-  database: 'notes_app' });*/
-
+  database: 'notes_app' });
+*/
 db.connect((err) => {
   if (err) {
     console.error('Koneksi ke database gagal:', err);
