54cbe2c82db2f006cfb6e3c78b0e748a0194aad6
goodman97 <151006064+goodman97@users.noreply.github.com>
2026-05-11 17:06:24 +0700

    perbaikan

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const notesRoutes = require('./routes/notes');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
